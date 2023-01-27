1be1a5409dfcd4f7a37620b0e5a83fd45b9c45c6
Julien Houget <Julien.Houget.Etu@univ-lemans.fr>
2023-01-27 11:08:36 +0100

    Crée une librairie commune à tout le projet.
    Définit un type booléen.

---
+++ include/commun.h
@@ -0,0 +1,18 @@
+/**
+ * \file commune.h
+ * \brief Librairie commune à tout le projet.
+ * \author Julien Houget
+ * \date 27/01/22
+ */
+
+#ifndef _COMMUN_H_
+#define _COMMUN_H_
+
+#include <stdio.h>
+
+/**
+ * \brief Définition du type booléen.
+ */
+typedef enum booleen_s { FAUX, VRAI } booleen_t;
+
+#endif
