ed6570d557bd4aed57f0b2ec93fd6d750f1c2c5d
Hibon Clement <s196128@ic2s120-02.univ-lemans.fr>
2023-02-03 16:32:44 +0100

    Début des entités

--- include/entite.h
+++ include/entite.h
@@ -1,10 +1,13 @@
-/*
- * \file 
- *
- * \brief
- *
- * \author
+/**
+ * @file entite.h
+ * 
+ * @brief 
+ * 
+ * @author Clément Hibon
+ * @date 3 février
+ * @version 1.1
  */
+
 
 
 
@@ -15,14 +18,22 @@
 
 
 
-/*
- * \struct t_entite
- * \brief Structure modélisant une entité
+/**
+ * @struct t_entite
+ * @brief Structure modélisant une entité
  */
 typedef struct s_entite {
     #include <attributs_entite.h>
+    t_vecteur2 position;
 } t_entite;
 
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  Fonctions                                 */
+/* -------------------------------------------------------------------------- */
 
 
 t_entite* creerEntite();
---
+++ src/entite.c
@@ -0,0 +1,35 @@
+/**
+ * @file entite.c
+ * 
+ * @brief 
+ * 
+ * @author Clément Hibon
+ * @date 3 février
+ * @version 1.1
+ */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/entite.h"
+#include "../include/utils.h"
+
+
+
+
+
+t_entite* creerEntite(const t_vecteur2 position) {
+    t_entite *entite = malloc(sizeof(t_entite));
+
+    return entite;
+}
+
+
+
+
+
+void deplacerEntite(t_entite entite) {
+
+}
