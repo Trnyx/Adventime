b07f451638717bd52cca1f83276e42c95f61731f
Julien Houget <Julien.Houget.Etu@univ-lemans.fr>
2023-02-03 15:24:37 +0100

    Ajoute le fichier oublié pour le dernier commit.

--- include/attributs_item.h
+++ include/attributs_item.h
@@ -16,5 +16,5 @@ char* nom; /**<Nom de l'item*/
 /**
  * POINTEURS DE FONCTION
  */
-void (*afficher)(t_item* const);
-void (*detruire)(t_item**);
+t_err (*afficher)(t_item* const);
+t_err (*detruire)(t_item**);
