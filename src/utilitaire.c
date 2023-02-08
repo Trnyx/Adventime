c6a1face473f9a7b0dc2ae724debe94ecc1ab462
BlastGhost <47760486+BlastGhost@users.noreply.github.com>
2023-02-08 18:53:33 +0100

    Ajout de la fonction calcul de distance entre deux points

---
+++ src/utilitaire.c
@@ -0,0 +1,54 @@
+/**
+ * @file utilitaire.c
+ *
+ * @brief
+ *
+ * @author Clément Hibon
+ * @date 21 janvier
+ * @version 1.3
+ */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "../include/utilitaire.h"
+
+
+
+
+
+/**
+ * @brief Get the Nombre Aleatoire object
+ * 
+ * @param min 
+ * @param max 
+ * @return int 
+ */
+int getNombreAleatoire(const int min, const int max) {
+    return min + rand() % ((max + 1) - min);
+}
+
+
+
+int getNombreAvecProbabilite() {
+    return 0;
+}
+
+
+
+
+
+/**
+ * @brief 
+ * 
+ * @param source 
+ * @param cible 
+ * @return float 
+ */
+float calculDistanceEntrePoints(const t_vecteur2 source, const t_vecteur2 cible) {
+    return sqrt( pow(cible.x - source.x, 2) + pow(cible.y - source.y, 2) );
+}
