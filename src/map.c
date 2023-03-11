893914837bfd66df319099a4208871ebb73162a3
BlastGhost <47760486+BlastGhost@users.noreply.github.com>
2023-03-11 01:00:48 +0100

    Fix Affichage Arbre

--- src/map.c
+++ src/map.c
@@ -163,8 +163,8 @@ void dessinerSol(t_map *map) {
     rendu.w = camera->tailleRendu.x;
     rendu.h = camera->tailleRendu.y;
 
-    for (float x = camera->origine.x - 1; x <= camera->position.x + TAILLE_CAMERA_DEMI_LARGEUR + 1; x++) {
-        for (float y = camera->origine.y - 1; y <= camera->position.y + TAILLE_CAMERA_DEMI_HAUTEUR + 1; y++) {
+    for (float x = camera->origine.x - 1; x <= camera->position.x + TAILLE_CAMERA_DEMI_LARGEUR + 2; x++) {
+        for (float y = camera->origine.y - 1; y <= camera->position.y + TAILLE_CAMERA_DEMI_HAUTEUR + 2; y++) {
             rendu.x = positionRelativeEnPositionSurEcran(x, camera->offset.x, camera->origine.x, rendu.w);
             rendu.y = positionRelativeEnPositionSurEcran(y, camera->offset.y, camera->origine.y, rendu.h);
 
@@ -244,8 +244,8 @@ void dessinerVegetation(t_map *map) {
 
     for (float x = camera->origine.x - 1; x <= camera->position.x + TAILLE_CAMERA_DEMI_LARGEUR + 1; x++) {
         for (float y = camera->origine.y - 1; y <= camera->position.y + TAILLE_CAMERA_DEMI_HAUTEUR + 1; y++) {
-            rendu.x = positionRelativeEnPositionSurEcran(x, camera->offset.x, camera->origine.x, rendu.w) + rendu.w;
-            rendu.y = positionRelativeEnPositionSurEcran(y, camera->offset.y, camera->origine.y, rendu.h) + rendu.h;
+            rendu.x = positionRelativeEnPositionSurEcran(x, camera->offset.x, camera->origine.x, rendu.w);
+            rendu.y = positionRelativeEnPositionSurEcran(y, camera->offset.y, camera->origine.y, rendu.h);
 
 
             t_block *block = getBlockDansMap(x, y, COUCHE_OBJETS, map);
